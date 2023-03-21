9a083917fb45be353d09810342a74437ea02d00b
Maximenco Vladislav <vladislavmaksimenko71@gmail.com>
2023-03-21 13:06:31 +0200

    Evaluare 2

---
+++ Evaluare_2/Sarcina 2/ProductForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <form @submit.prevent="addProduct">
+    <label for="product">Produs:</label>
+    <select id="product" v-model="selectedProduct">
+      <option v-for="product in products" :value="product">{{ product.name }}</option>
+    </select>
+    <label for="price">Preț:</label>
+<input type="number" id="price" v-model.number="price" min="0" step="0.01">
+
+<label for="quantity">Cantitate:</label>
+<input type="number" id="quantity" v-model.number="quantity" min="1">
+
+<button type="submit">Adaugă produs</button>
+<button type="button" @click="resetForm">Reset</button>
---
+++ Evaluare_2/Sarcina 2/produs.js
@@ -0,0 +1,7 @@
+export const products = [
+    { name: 'Produs 1', price: 10 },
+    { name: 'Produs 2', price: 20 },
+    { name: 'Produs 3', price: 30 },
+    { name: 'Produs 4', price: 40 },
+    { name: 'Produs 5', price: 50 },
+    ];
